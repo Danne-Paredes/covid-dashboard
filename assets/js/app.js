2d9d20086abbb7f1dc4d1fa7e2e1baece655bb83
Danne <danneparedes@gmail.com>
2020-08-05 22:21:42 -0700

    added sample map

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,26 @@
 // code here
 
+// Creating map object
+var myMap = L.map("map", {
+    center: [40.7128, -74.0059],
+    zoom: 11
+  });
+  
+  // Adding tile layer
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
+  
+  // Use this link to get the geojson data.
+  var link = "static/data/nyc.geojson";
+  
+  // Grabbing our GeoJSON data..
+  d3.json(link, function(data) {
+    // Creating a GeoJSON layer with the retrieved data
+    L.geoJson(data).addTo(myMap);
+  });
