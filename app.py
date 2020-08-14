daa05aff4922b141a01b7d21344c5ce32257c0fc
SethAbbott916 <sethcode91@gmail.com>
2020-08-13 20:02:51 -0700

    working app.py

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, Blueprint, jsonify
 from flask_pymongo import PyMongo
-# import scrape_mars
 
+
+# Create an instance of Flask
 app = Flask(__name__)
 
-# Use flask_pymongo to set up mongo connection
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
-# mongo = PyMongo(app)
+# Use PyMongo to establish Mongo connection
+mongo = PyMongo(app, uri="mongodb://localhost:27017/covid-db")
 
-# Or set inline
-# mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+@app_after_request
+def after_request(response):
+    header = response.headers
+    header["Access-control-allow-origin"] = '*'
+    return response
 
 
 @app.route("/")
@@ -18,13 +21,19 @@ def index():
     return render_template("index.html")
 
 
-# @app.route("/scrape")
-# def scrape():
-#     mars = mongo.db.mars
-#     mars_data = scrape_mars.scrape_all()
-#     mars.replace_one({}, mars_data, upsert=True)
-#     return "Scraping Successful!"
+# Route that will trigger the scrape function
+@app.route("/api")
+def api():
+
+    covid-data = mongo.db.collection.find({}, {'_id'= False})
+    cases = [case for case in covid-data]
+    data = {
+
+    "cases": cases
+    }
+
+    return jsonify(data)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
